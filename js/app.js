07f48aeb92f0091e565daed94a3a4c0852c2a41c
AndreyrM01 <rjostin954@gmail.com>
2023-11-28 15:56:34 -0600

    feedback

--- js/app.js
+++ js/app.js
@@ -1,75 +1,6 @@
-// const header = document.querySelector("header");
 
-// const first_skill = document.querySelector(".skill:first-child");
-// const sk_counters = document.querySelectorAll(".counter span");
-// const progress_bars = document.querySelectorAll(".skill svg circle");
-
-// window.addEventListener("scroll", () => {
-//   if(!skillsPlayed) skillsCounter();
-// })
-
-// function stickyNavbar(){
-//   header.classList.toggle("scrolled", window.scrollY > 0);
-// }
-
-// stickyNavbar();
-
-// window.addEventListener("scroll", stickyNavbar);
-
-// // Reveal Animation
-
-// let sr = ScrollReveal({
-//   duration: 2500,
-//   distance: "60px",
-// });
-
-// sr.reveal(".showcase-info", { delay: 400 });
-// sr.reveal(".showcase-image", { origin:"top", delay: 500 });
-
-// // Skills Progress Bar
-
-// function hasReached(el){
-//   let topPosition = el.getBoundingClientRect().top;
-
-//   if(window.innerHeight >= topPosition + el.offsetHeight) return true;
-//   return false;
-// }
-
-// function updateCount(num, maxNum){
-//   let currentNum = +num.innerText;
-  
-//   if(currentNum < maxNum){
-//     num.innerHTML = ++currentNum;
-//     setTimeout(() => {
-//       updateCount(num, maxNum);
-//     }, 18);
-//   }
-// }
-
-// let skillsPlayed = false;
-
-// function skillsCounter(){
-//     if(!hasReached(first_skill)) return;
-
-//     skillsPlayed = true;
-  
-//   sk_counters.forEach((counter, i) => {
-//     let target = +counter.dataset.target;
-//     let strokeValue = 427 - 427 * (target / 100);
-
-//     progress_bars[i].style.setProperty("--target", strokeValue);
-
-//     setTimeout(() =>{
-//       updateCount(counter, target);
-//     }, 400);
-//   })
-
-//   progress_bars.forEach(
-//     (p) => (p.style.animation = "progress 2s ease-in-out forwards"));
-// }
 const header = document.querySelector("header");
 
-const first_skill = document.querySelector(".skill:first-child");
 const sk_counters = document.querySelectorAll(".counter span");
 const progress_bars = document.querySelectorAll(".skills svg circle");
 
@@ -121,52 +52,6 @@ let sr = ScrollReveal({
 
 sr.reveal(".showcase-info", { delay: 600 });
 sr.reveal(".showcase-image", { origin: "top", delay: 700 });
-
-/* --------------- Skills Progress Bar Animation --------------- */
-
-// function hasReached(el) {
-//   let topPosition = el.getBoundingClientRect().top;
-
-//   if (window.innerHeight >= topPosition + el.offsetHeight) return true;
-//   return false;
-// }
-function hasReached(el) {
-  if (el === null) {
-    console.log("Error: Element is null.");
-    return false;
-  }
-
-  let topPosition = el.getBoundingClientRect().top;
-
-  if (window.innerHeight >= topPosition + el.offsetHeight) return true;
-  return false;
-}
-
-
-let skillsPlayed = false;
-
-function skillsCounter() {
-  if (!hasReached(first_skill)) return;
-
-  skillsPlayed = true;
-
-  sk_counters.forEach((counter, i) => {
-    let target = +counter.dataset.target;
-    let strokeValue = 427 - 427 * (target / 100);
-
-    progress_bars[i].style.setProperty("--target", strokeValue);
-
-    setTimeout(() => {
-      updateCount(counter, target);
-    }, 400);
-  });
-
-  progress_bars.forEach(
-    (p) => (p.style.animation = "progress 2s ease-in-out forwards")
-  );
-}
-
-skillsCounter();
 
 /* --------------- Modal Pop Up Animation Animation --------------- */
 
